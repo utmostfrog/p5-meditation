52e911c6952c5b878b4773a61ed4d175c8d1f239
Ruy Carpenter <ruycarpenter@gmail.com>
2018-10-06 13:49:46 -0700

    Added tensor flow but didn't work

--- meditate.js
+++ meditate.js
@@ -10,6 +10,10 @@ var alpha = 10; // how opaque is the tracing system
 
 var trace = true; // are we tracing?
 
+let video;
+let poseNet;
+let poses = [];
+
 function setup() {
   canvas = createCanvas(window.innerWidth, window.innerHeight);
 
@@ -19,14 +23,41 @@ function setup() {
   for (var i = 0; i<sines.length; i++) {
     sines[i] = PI; // start EVERYBODY facing NORTH
   }
+
+  video = createCapture(VIDEO);
+  video.size(width, height);
+
+  // Create a new poseNet method with a single detection
+  poseNet = ml5.poseNet(video, modelReady);
+  // This sets up an event that fills the global variable "poses"
+  // with an array every time new poses are detected
+  poseNet.on('pose', function(results) {
+    poses = results;
+  });
+  // Hide the video element, and just show the canvas
+  video.hide();
+
+}
+
+function modelReady() {
+  select('#status').html('Model Loaded');
 }
 
 function draw() {
   if (!trace) {
     background(204); // clear screen if showing geometry
     stroke(0, 255); // black pen
+    strokeWidth(3);
     noFill(); // don't fill
   }
+
+    // push();
+    // image(video, 0, 0, width, height);
+    //
+    // // We can call both functions to draw all keypoints and the skeletons
+    // drawKeypoints();
+    // drawSkeleton();
+    // pop();
 
   // MAIN ACTION
   push(); // start a transformation matrix
@@ -37,22 +68,23 @@ function draw() {
     // setup for tracing
     if (trace) {
       stroke(0, 0, 255*(float(i)/sines.length), alpha); // blue
-      fill(0, 0, 255, alpha/2); // also, um, blue
+      fill(0, 0, 255, alpha/4); // also, um, blue
       erad = 5.0*(1.0-float(i)/sines.length); // pen width will be related to which sine
     }
     var radius = rad/(i+1); // radius for circle itself
     rotate(sines[i]); // rotate circle
-    if (!trace) ellipse(0, 0, radius*2, radius*2); // if we're simulating, draw the sine
+    if (!trace) ellipse(0, 0, radius*5, radius*5); // if we're simulating, draw the sine
     push(); // go up one level
     translate(0, radius); // move to sine edge
-    if (!trace) ellipse(0, 0, 5, 5); // draw a little circle
-    if (trace) ellipse(0, 0, erad, erad); // draw with erad if tracing
+    if (!trace) ellipse(5, 5, 50, 50); // draw a little circle
+    if (trace) ellipse(0, 0, erad+25, erad+25); // draw with erad if tracing
     pop(); // go down one level
     translate(0, radius); // move into position for next sine
-    sines[i] = (sines[i]+(fund+(fund*i*ratio)))%TWO_PI; // update angle based on fundamental
+    sines[i] = (sines[i]+(fund+(fund*i*(ratio*PI))))%TWO_PI; // update angle based on fundamental
   }
 
   pop(); // pop down final transformation
+
 
 }
 
@@ -65,4 +97,44 @@ function keyReleased() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+}
+
+/* ===
+ml5 Example
+PoseNet example using p5.js
+=== */
+
+
+// A function to draw ellipses over the detected keypoints
+function drawKeypoints()  {
+  // Loop through all the poses detected
+  for (let i = 0; i < poses.length; i++) {
+    // For each pose detected, loop through all the keypoints
+    let pose = poses[i].pose;
+    for (let j = 0; j < pose.keypoints.length; j++) {
+      // A keypoint is an object describing a body part (like rightArm or leftShoulder)
+      let keypoint = pose.keypoints[j];
+      // Only draw an ellipse is the pose probability is bigger than 0.2
+      if (keypoint.score > 0.2) {
+        fill(255, 0, 0);
+        noStroke();
+        ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
+      }
+    }
+  }
+}
+
+// A function to draw the skeletons
+function drawSkeleton() {
+  // Loop through all the skeletons detected
+  for (let i = 0; i < poses.length; i++) {
+    let skeleton = poses[i].skeleton;
+    // For every skeleton, loop through all body connections
+    for (let j = 0; j < skeleton.length; j++) {
+      let partA = skeleton[j][0];
+      let partB = skeleton[j][1];
+      stroke(255, 0, 0);
+      line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
+    }
+  }
 }
